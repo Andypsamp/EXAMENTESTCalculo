449467171d037fdf53ef78dde612c22b742b134d
apenasampedro <apenasampedro@atanasoft09.danielcastelao.org>
2015-06-02 12:13:06 +0200

    commitfinal

--- test/mcd/Mensaje.java
+++ test/mcd/Mensaje.java
@@ -38,16 +38,5 @@ public class Mensaje {
     public void tearDown() {
     }
    public void testObtenerMCD() {
-        System.out.println("obtenerMCD");
-        int num1 = 0;
-        int num2 = 0;
-        String expResult = "ERROR exception";
-        Calculo instance = new Calculo(); 
-        instance.setNumero1(num1);
-        instance.setNumero2(num2);
-        instance.obtenerMCD(num1, num2);
-        String result = instance.getMensajeResultado();
-        assertEquals(expResult, result);
-    }
-
+   }
 }
